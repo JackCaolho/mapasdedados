8ae1ac771e5d3de460c574f5fdba7af052e323ee
GuilhermeSouza2 <guilherme.souza2071@gmail.com>
2024-06-04 18:24:41 -0300

    Tratamento de base + novo gráfico
    Somei duas planilhas e fiz um novo gráfico em pizza que ainda está incompleto

--- Dash.py
+++ Dash.py
@@ -4,40 +4,49 @@ import pandas as pd
 import os
 from flask import Flask, send_from_directory
 
-# Caminho do arquivo
+# Caminho dos arquivos
 caminho_arquivo1 = os.path.join('bases-nao-tratadas', 'Telefonia fixa', 'Acessos_Telefonia_Fixa_Total.csv')
-
-df1 = pd.read_csv(caminho_arquivo1, sep=';', encoding='utf-8')
-
 caminho_arquivo2 = os.path.join('bases-nao-tratadas', 'Telefonia fixa', 'Acessos_Telefonia_Fixa_Autorizadas.csv')
-
 caminho_arquivo3 = os.path.join('bases-nao-tratadas', 'Telefonia fixa', 'Acessos_Telefonia_Fixa_Concessionarias_2021-2022_Colunas.csv')
+caminho_arquivo4 = os.path.join('bases-nao-tratadas', 'Telefonia fixa', 'Acessos_Telefonia_Fixa_Concessionarias_2007_2020_Colunas.csv')
 
-
-# Carregar o DataFrame
-df2_sp_agrupado = pd.read_csv(caminho_arquivo2, sep=';', encoding='utf-8') \
-                    .query('UF == "SP"') \
-                    .groupby(['Ano', 'Mês']) \
-                    .agg({'Acessos': 'sum'}) \
-                    .reset_index() \
-                    .rename(columns={'Acessos': 'Total_Acessos'})
-
+# Leitura dos arquivos
+df1 = pd.read_csv(caminho_arquivo1, sep=';', encoding='utf-8')
+df2 = pd.read_csv(caminho_arquivo2, sep=';', encoding='utf-8').query('UF == "SP"')
 df3 = pd.read_csv(caminho_arquivo3, sep=';', encoding='utf-8')
+df4 = pd.read_csv(caminho_arquivo4, sep=';', encoding='utf-8')
 
+# Transformações no df3 e df4
 df3_melted = df3.melt(
-    id_vars=['Tipo de Outorga', 'CNPJ', 'Município', 'UF', 'Empresa', 'Porte da Prestadora', 'Tipo de Pessoa','Tipo do Acesso' ,'Tipo de Atendimento', 'Código IBGE Município', 'Grupo Econômico' ],
+    id_vars=['Tipo de Outorga', 'CNPJ', 'Município', 'UF', 'Empresa', 'Porte da Prestadora', 'Tipo de Pessoa', 'Tipo do Acesso', 'Tipo de Atendimento', 'Código IBGE Município', 'Grupo Econômico'],
     var_name='Data',
     value_name='Acessos'
 )
-
-# Dividir a coluna 'Data' em 'Ano' e 'Mês'
 df3_melted[['Ano', 'Mês']] = df3_melted['Data'].str.split('-', expand=True)
-
-# Remover a coluna 'Data'
 df3_melted.drop(columns=['Data'], inplace=True)
+df3_melted['Ano'] = df3_melted['Ano'].astype(int)
+df3_melted['Mês'] = df3_melted['Mês'].astype(int)
 
-print(df3_melted.head(3))
+df4_melted = df4.melt(
+    id_vars=['Tipo de Outorga', 'CNPJ', 'Município', 'UF', 'Empresa', 'Porte da Prestadora', 'Tipo do Acesso', 'Código IBGE Município', 'Grupo Econômico'],
+    var_name='Data',
+    value_name='Acessos'
+)
+df4_melted[['Ano', 'Mês']] = df4_melted['Data'].str.split('-', expand=True)
+df4_melted.drop(columns=['Data'], inplace=True)
+df4_melted['Ano'] = df4_melted['Ano'].astype(int)
+df4_melted['Mês'] = df4_melted['Mês'].astype(int)
 
+df_conc = pd.concat([df3_melted, df4_melted], ignore_index=True)
+
+# Agrupar e somar acessos em df2
+df_agrupado = df2.groupby(['Ano', 'Mês', 'Tipo de Outorga']).agg({'Acessos': 'sum'}).reset_index().rename(columns={'Acessos': 'Total_Acessos'})
+
+# Agrupar e somar acessos em df3_melted e df4_melted
+df_agrupado2 = df_conc.groupby(['Ano', 'Mês', 'Tipo de Outorga']).agg({'Acessos': 'sum'}).reset_index().rename(columns={'Acessos': 'Total_Acessos'})
+
+# Concatenar os dataframes
+df_final = pd.concat([df_agrupado, df_agrupado2], ignore_index=True)
 
 # Inicializar o servidor Flask
 server = Flask(__name__)
@@ -60,14 +69,11 @@ def serve_html_sabadoanimado():
 # Inicializar o aplicativo Dash
 app = Dash(__name__, server=server)
 
-# Criar a figura
-fig1 = px.bar(df1, x="Ano", y="Acessos", barmode="group")
-
-fig2 = px.bar(df2_sp_agrupado, x="Ano", y="Total_Acessos", barmode="group" )
-
 # Obter opções únicas
-opcoes = list(df1['Tipo de Outorga'].unique())
+opcoes = list(df_final['Tipo de Outorga'].unique())
 opcoes.append("Geral")
+
+anos = sorted(df1['Ano'].unique())
 
 # Layout do aplicativo
 app.layout = html.Div(children=[
@@ -76,15 +82,13 @@ app.layout = html.Div(children=[
 
     dcc.Dropdown(options=[{'label': opt, 'value': opt} for opt in opcoes], value='Geral', id='Tipo de Outorga'),
 
-    dcc.Graph(
-        id='Acessos_Telefonia_Fixa_Total',
-        figure=fig1
-    ),
+    dcc.Dropdown(options=[{'label': ano, 'value': ano} for ano in anos], value=anos[0], id='ano-dropdown'),
 
-    dcc.Graph(
-        id='Acessos_Telefonia_Fixa_Autorizadas',
-        figure=fig2
-    ),
+    dcc.Graph(id='Acessos_Telefonia_Fixa_Total'),
+
+    dcc.Graph(id='Acessos_Telefonia_Fixa_SP'),
+
+    dcc.Graph(id='pie-chart'),
 
     html.Br(),
 
@@ -102,18 +106,45 @@ app.layout = html.Div(children=[
     )
 ])
 
-# Callback para atualizar o gráfico
+# Callback para atualizar o gráfico Acessos_Telefonia_Fixa_Total
 @app.callback(
     Output('Acessos_Telefonia_Fixa_Total', 'figure'),
     [Input('Tipo de Outorga', 'value')]
 )
-def update_output(value):
+def update_total(value):
     if value == "Geral":
         fig = px.bar(df1, x="Ano", y="Acessos", barmode="group")
     else:
         tabela_filtrada = df1.loc[df1['Tipo de Outorga'] == value, :]
         fig = px.bar(tabela_filtrada, x="Ano", y="Acessos", color="Tipo de Outorga", barmode="group")
     return fig
+
+# Callback para atualizar o gráfico Acessos_Telefonia_Fixa_SP
+@app.callback(
+    Output('Acessos_Telefonia_Fixa_SP', 'figure'),
+    [Input('Tipo de Outorga', 'value')]
+)
+def update_sp(value1):
+    if value1 == "Geral":
+        fig2 = px.bar(df_final, x="Ano", y="Total_Acessos", barmode="group")
+    else:
+        tabela_filtrada = df_final.loc[df_final['Tipo de Outorga'] == value1, :]
+        fig2 = px.bar(tabela_filtrada, x="Ano", y="Total_Acessos", color="Tipo de Outorga", barmode="group")
+    return fig2
+
+# Callback para atualizar o gráfico de pizza
+@app.callback(
+    Output('pie-chart', 'figure'),
+    [Input('ano-dropdown', 'value')]
+)
+def update_pie_chart(selected_year):
+    # Filtrar o DataFrame pelo ano selecionado
+    filtered_df = df2[df2['Ano'] == selected_year]
+
+    # Criar o gráfico de pizza
+    fig3 = px.pie(filtered_df, names='Empresa', values='Acessos', title=f'Distribuição de Acessos por Empresa em {selected_year}')
+    
+    return fig3
 
 if __name__ == '__main__':
     app.run_server(debug=True)
