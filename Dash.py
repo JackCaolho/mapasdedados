8c6d28ce8611d1c81b40b853c5ea93846523b830
GuilhermeSouza2 <guilherme.souza2071@gmail.com>
2024-06-05 17:53:44 -0300

    Melhoramento no gráfico de pizza
    Filtrei as top 9 empresas e coloquei o resto como "OUTROS"

--- Dash.py
+++ Dash.py
@@ -9,12 +9,15 @@ caminho_arquivo1 = os.path.join('bases-nao-tratadas', 'Telefonia fixa', 'Acessos
 caminho_arquivo2 = os.path.join('bases-nao-tratadas', 'Telefonia fixa', 'Acessos_Telefonia_Fixa_Autorizadas.csv')
 caminho_arquivo3 = os.path.join('bases-nao-tratadas', 'Telefonia fixa', 'Acessos_Telefonia_Fixa_Concessionarias_2021-2022_Colunas.csv')
 caminho_arquivo4 = os.path.join('bases-nao-tratadas', 'Telefonia fixa', 'Acessos_Telefonia_Fixa_Concessionarias_2007_2020_Colunas.csv')
+caminho_arquivo5 = os.path.join('bases-nao-tratadas', 'Telefonia fixa', 'CSV_PORTABILIDADE.csv')
+
 
 # Leitura dos arquivos
 df1 = pd.read_csv(caminho_arquivo1, sep=';', encoding='utf-8')
 df2 = pd.read_csv(caminho_arquivo2, sep=';', encoding='utf-8').query('UF == "SP"')
 df3 = pd.read_csv(caminho_arquivo3, sep=';', encoding='utf-8')
 df4 = pd.read_csv(caminho_arquivo4, sep=';', encoding='utf-8')
+df5 = pd.read_csv(caminho_arquivo5, sep=';', encoding='utf-8').query('SG_UF == "SP"')
 
 # Transformações no df3 e df4
 df3_melted = df3.melt(
@@ -39,11 +42,30 @@ df4_melted['Mês'] = df4_melted['Mês'].astype(int)
 
 df_conc = pd.concat([df3_melted, df4_melted], ignore_index=True)
 
-# Agrupar e somar acessos em df2
+
+#Preencher as colunas vazias
+df2['Tipo de Pessoa'] = df2['Tipo de Pessoa'].fillna('Desconhecido')
+
+# Agrupar e somar acessos em df
 df_agrupado = df2.groupby(['Ano', 'Mês', 'Tipo de Outorga']).agg({'Acessos': 'sum'}).reset_index().rename(columns={'Acessos': 'Total_Acessos'})
 
-# Agrupar e somar acessos em df3_melted e df4_melted
 df_agrupado2 = df_conc.groupby(['Ano', 'Mês', 'Tipo de Outorga']).agg({'Acessos': 'sum'}).reset_index().rename(columns={'Acessos': 'Total_Acessos'})
+
+df_agrupado3 = df2.groupby(['Ano', 'Mês', 'Empresa', 'Tipo de Pessoa']).agg({'Acessos': 'sum'}).reset_index().rename(columns={'Acessos': 'Total_Acessos_Empresa'})
+
+# Função para agrupar empresas menores e agrupar em "OUTROS" por ano
+def agrupar_empresas_por_ano(df, top_n=9):
+    df_t10 = pd.DataFrame()
+    for ano in df['Ano'].unique():
+        df_ano = df[df['Ano'] == ano]
+        total_por_empresa = df_ano.groupby('Empresa')['Total_Acessos_Empresa'].sum().sort_values(ascending=False)
+        top_empresas = total_por_empresa.head(top_n).index
+        df_ano.loc[~df_ano['Empresa'].isin(top_empresas), 'Empresa'] = 'OUTROS'
+        df_ano = df_ano.groupby(['Ano', 'Mês', 'Empresa', 'Tipo de Pessoa']).agg({'Total_Acessos_Empresa': 'sum'}).reset_index()
+        df_t10 = pd.concat([df_t10, df_ano], ignore_index=True)
+    return df_t10
+
+df_agrupado3 = agrupar_empresas_por_ano(df_agrupado3)
 
 # Concatenar os dataframes
 df_final = pd.concat([df_agrupado, df_agrupado2], ignore_index=True)
@@ -51,7 +73,7 @@ df_final = pd.concat([df_agrupado, df_agrupado2], ignore_index=True)
 # Inicializar o servidor Flask
 server = Flask(__name__)
 
-# Servir o arquivo HTML
+# Caminho dos HeatMap
 @server.route('/40028922')
 def serve_html_40028922():
     return send_from_directory(
@@ -73,7 +95,11 @@ app = Dash(__name__, server=server)
 opcoes = list(df_final['Tipo de Outorga'].unique())
 opcoes.append("Geral")
 
-anos = sorted(df1['Ano'].unique())
+anos = sorted(df_agrupado3['Ano'].unique())
+anos.insert(0, "Geral")
+
+tipos_pessoa = list(df_agrupado3['Tipo de Pessoa'].unique())
+tipos_pessoa.append("Geral")
 
 # Layout do aplicativo
 app.layout = html.Div(children=[
@@ -82,7 +108,9 @@ app.layout = html.Div(children=[
 
     dcc.Dropdown(options=[{'label': opt, 'value': opt} for opt in opcoes], value='Geral', id='Tipo de Outorga'),
 
-    dcc.Dropdown(options=[{'label': ano, 'value': ano} for ano in anos], value=anos[0], id='ano-dropdown'),
+    dcc.Dropdown(options=[{'label': ano, 'value': ano} for ano in anos], value='Geral', id='ano-dropdown'),
+
+    dcc.Dropdown(options=[{'label': tipo, 'value': tipo} for tipo in tipos_pessoa], value='Geral', id='tipo-pessoa-dropdown'),
 
     dcc.Graph(id='Acessos_Telefonia_Fixa_Total'),
 
@@ -92,7 +120,7 @@ app.layout = html.Div(children=[
 
     html.Br(),
 
-    # Botão que redireciona para outra página
+    # Botão que redireciona para os Heatmap
     html.A(
         html.Button('Heatmap tel fixo', id='link-button-1'),
         href='/40028922',
@@ -113,6 +141,7 @@ app.layout = html.Div(children=[
 )
 def update_total(value):
     if value == "Geral":
+        #Tipo de gráfico
         fig = px.bar(df1, x="Ano", y="Acessos", barmode="group")
     else:
         tabela_filtrada = df1.loc[df1['Tipo de Outorga'] == value, :]
@@ -126,6 +155,7 @@ def update_total(value):
 )
 def update_sp(value1):
     if value1 == "Geral":
+        #Tipo de gráfico
         fig2 = px.bar(df_final, x="Ano", y="Total_Acessos", barmode="group")
     else:
         tabela_filtrada = df_final.loc[df_final['Tipo de Outorga'] == value1, :]
@@ -135,16 +165,22 @@ def update_sp(value1):
 # Callback para atualizar o gráfico de pizza
 @app.callback(
     Output('pie-chart', 'figure'),
-    [Input('ano-dropdown', 'value')]
+    [Input('ano-dropdown', 'value'), Input('tipo-pessoa-dropdown', 'value')]
 )
-def update_pie_chart(selected_year):
-    # Filtrar o DataFrame pelo ano selecionado
-    filtered_df = df2[df2['Ano'] == selected_year]
+def update_pie_chart(selected_year, selected_tipo_pessoa):
+    filtered_df = df_agrupado3.copy()
 
-    # Criar o gráfico de pizza
-    fig3 = px.pie(filtered_df, names='Empresa', values='Acessos', title=f'Distribuição de Acessos por Empresa em {selected_year}')
+    if selected_year != "Geral":
+        filtered_df = filtered_df[filtered_df['Ano'] == selected_year]
+    
+    if selected_tipo_pessoa != "Geral":
+        filtered_df = filtered_df[filtered_df['Tipo de Pessoa'] == selected_tipo_pessoa]
+
+    # Tipo de gráfico
+    fig3 = px.pie(filtered_df, names='Empresa', values='Total_Acessos_Empresa', title=f'Distribuição de Acessos por Empresa em {selected_year}')
     
     return fig3
 
-if __name__ == '__main__':
+
+if __name__ == '__main__':  
     app.run_server(debug=True)
